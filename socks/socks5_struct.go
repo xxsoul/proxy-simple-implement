5d3caf0ec82f889d33e6b95de3ceab5f734cd15a
xinlei.xiao <xinlei.xiao@piaoshen.com>
2020-05-25 18:09:08 +0800

    socks5 base struct

---
+++ socks/socks5_struct.go
@@ -0,0 +1,33 @@
+package socks
+
+// Socks5AuthMethodRequest socks5代理认证方法请求
+type Socks5AuthMethodRequest struct {
+	Ver      byte
+	NMethods byte
+	Methods  []byte
+}
+
+// Socks5AuthMethodResponse socks5认证方法结果
+type Socks5AuthMethodResponse struct {
+	Ver    byte
+	Method byte
+}
+
+// Socks5ProxyRequest socks5代理请求明细结构
+type Socks5ProxyRequest struct {
+	Ver     byte
+	Cmd     byte
+	Rsv     byte
+	Atyp    byte
+	DstAddr []byte
+	DstPort byte
+}
+
+type Socks5ProxyResponse struct {
+	Ver     byte
+	Rep     byte
+	Rsv     byte
+	Atyp    byte
+	BndAddr []byte
+	BndPort byte
+}
